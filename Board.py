0c4f8211abda04b87e217371dde991cda309c2b3
kjbraakman <77073753+kjbraakman@users.noreply.github.com>
2021-02-08 17:32:09 +0100

    Edit board functionality

--- Board.py
+++ Board.py
@@ -3,6 +3,7 @@ from Piece import *
 class Board:
     def __init__(self, file_name):
         self.field = [['X' for i in range(6)] for i in range(4)]
+        self.moves = [[i,j] for i in range(4) for j in range(6)]
         setup_list = []
 
         with open(file_name) as f:
@@ -13,13 +14,34 @@ class Board:
                 if line != '':
                     setup_list.append(line.rstrip('\n'))
 
-        random_list = []
         for i in range(4):
             line = setup_list[i]
             help_list = line.split()
             for j in help_list:
                 if j != 'X':
                     self.field[i][help_list.index(j)] = Piece(j[0], [i, help_list.index(j)], j[1:])
+
+    def possible_moves(self, player):
+        player_pieces = []
+        for line in self.field:
+            for piece in line:
+                if piece != 'X':
+                    if piece.team == player:
+                        player_pieces.append(piece.position)
+
+        possible_plays = []
+        for line in self.field:
+            for piece in line:
+                if piece != 'X':
+                    if piece.team == player:
+                        if piece.rank != 'F' or piece.rank != 'B':
+                            if [piece.position[0] - 1, piece.position[1]] in self.moves and [piece.position[0] - 1, piece.position[1]] not in player_pieces:
+                                possible_plays.append([piece.rank, [piece.position[0] - 1, piece.position[1]]])
+                            if [piece.position[0] + 1, piece.position[1]] in self.moves and [piece.position[0] + 1, piece.position[1]] not in player_pieces:
+                                possible_plays.append([piece.rank, [piece.position[0] + 1, piece.position[1]]])
+                            if [piece.position[0], piece.position[1] + 1] in self.moves and [piece.position[0], piece.position[1] + 1] not in player_pieces:
+                                possible_plays.append([piece.rank, [piece.position[0], piece.position[1] + 1]])
+        return possible_plays
 
     def print_board(self):
         for i in range(4):
@@ -32,4 +54,20 @@ class Board:
 
 
 field = Board("Initial_setup.txt")
-print(field)
+# possible_plays = {}
+# for lin in field.field:
+#     for piece in lin:
+#         if piece != 'X':
+#             if piece.team == 'R':
+#                 if piece.rank != 'F' or piece.rank != 'B':
+#                     possible_plays[piece] = []
+#                     if [piece.position[0] - 1, piece.position[0]] in field.moves:
+#                         print('ya')
+#                         possible_plays.get(piece).append(piece.position[0] - 1)
+#                     if piece.position[0] + 1 in field.moves:
+#                         possible_plays.get(piece).append(piece.position[0] + 1)
+#                     if piece.position[1] + 1 in field.moves:
+#                         possible_plays.get(piece).append(piece.position[1] + 1)
+#
+# print(possible_plays)
+print(field.possible_moves('R'))
--- Game.py
+++ Game.py
@@ -1,4 +1,28 @@
 from Piece import *
+from Board import *
+import random as random
 
 class Game:
-    def __init__(self):
+    def __init__(self, filename):
+        self.board = Board(filename)
+
+    def move(self, player):
+        moves = self.board.possible_moves(player)
+        move = random.choice(moves)
+        return move
+
+    def play_game(self):
+        players = ['R', 'B']
+        i = 0
+        while True:
+            move = self.move(players[i % 2])
+            self.board.field[][] = 'X'
+
+            i += 1
+
+        return victor
+
+while not victory
+    move()
+    return victor
+
