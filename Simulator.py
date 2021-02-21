9260bec3fac1096d185897a3d5c21ac0a2ccb622
PatrikPat <p.roest@student.tue.nl>
2021-02-21 13:25:40 +0100

    Added logic to make setups. Still incomplete!

--- Simulator.py
+++ Simulator.py
@@ -53,29 +53,100 @@ for i in range(4):
             else:
                 setup_field[i][help_list.index(j)] = Piece(j[0], [i, help_list.index(j)], int(j[1:]))
 
+flag_positions = [[0, 0], [3, 0]]
+bomb_positions = [[[0, 1], [1, 0]], [[2, 0], [1, 3]]]
+miner_positions = [[0, 0], [1, 0], [2, 0], [3, 0]]
+general_maarschalk = [9, 10]
+spy_verkenner = [2, 2, 1]
 possible_positions = [[i, j] for i in range(4) for j in range(2)]
 
-for flag in [[0, 0], [3, 0]]: #two possible flag positions
+# for i in range(2): #two possible flag positions
+#     flag = flag_positions[i]
+#     setup_field[flag[0]][flag[1]] = Piece('R', [flag[0], flag[1]], 'F')
+#     possible_positions.remove(flag)
+#     bomb_list = bomb_positions[i] #based on the flag, two possible bomb positions
+#     for j in range(2):
+#         bomb = bomb_list[j]
+#         setup_field[bomb[0][bomb[1]]] = Piece('R', [bomb[0], bomb[1]], 'B')
+#         possible_positions.remove(bomb)
+#         for miner in possible_positions and miner[0] == 0:
+#             setup_field[miner[0][miner[1]]] = Piece('R', [miner[0], miner[1]], 3)
+#            possible_positions.remove(miner)
+
+i = 0 #flag
+j = 0 #bomb
+z = 0 #miner
+y = 0 #general or maarschalk
+g = 0 #verkenner, verkenner, or spy
+general_placed = False
+all_fields = []
+
+while True:
+    possible_positions = [[i, j] for i in range(4) for j in range(2)]
+    miner_positions = [[0, 0], [1, 0], [2, 0], [3, 0]]
+    general_placed = False
+    g = 0
+
+    flag = flag_positions[i]
     setup_field[flag[0]][flag[1]] = Piece('R', [flag[0], flag[1]], 'F')
     possible_positions.remove(flag)
-    if flag == [0, 0]:
-        for bomb in [[0, 1], [1, 0]]: #based on the flag, two possible bomb positions
-            setup_field[bomb[0][bomb[1]]] = Piece('R', [bomb[0], bomb[1]], 'B')
-            possible_positions.remove(bomb)
-            for miner in possible_positions and miner[0] == 0:
-                setup_field[miner[0][miner[1]]] = Piece('R', [miner[0], miner[1]])
-                possible_positions.remove(miner)
+    miner_positions.remove(flag)
 
-sim = Simulator(10000)
-#red_prob, blue_prob, draw_prob, average_moves, average_moves_red, average_moves_blue, spies_prob = sim.runSimulation([0, 1])
-#print(red_prob, blue_prob, draw_prob, average_moves, average_moves_red, average_moves_blue, spies_prob)
-results = {}
+    bomb_list = bomb_positions[i]  # based on the flag, two possible bomb positions
+    bomb = bomb_list[j]
+    setup_field[bomb[0]][bomb[1]] = Piece('R', [bomb[0], bomb[1]], 'B')
+    possible_positions.remove(bomb)
+    if bomb[1] == 0:
+        miner_positions.remove(bomb)
 
-for i in range(4):
-    print(setup_field)
-    results[(0 + i, 1)] = list(sim.runSimulation([0 + i, 1], setup_field))
+    miner = miner_positions[z]
+    setup_field[miner[0]][miner[1]] = Piece('R', [miner[0], miner[1]], 3)
+    possible_positions.remove(miner)
+    
+    while True:
+        p = possible_positions[0]
+        if [p[0] + 2, p[1]] in possible_positions and p[1] == 1 and not general_placed:
+            setup_field[p[0]][p[1]] = Piece('R', [p[0], p[1]], general_maarschalk[y])
+            setup_field[p[0] + 2][p[1]] = Piece('R', [p[0] + 2, p[1]], general_maarschalk[y + 1])
+            possible_positions.remove([p[0], p[1]])
+            possible_positions.remove([p[0] + 2, p[1]])
+            general_placed = True
+        elif [p[0] - 2, p[1]] in possible_positions and p[1] == 1 and not general_placed:
+            setup_field[p[0]][p[1]] = Piece('R', [p[0], p[1]], general_maarschalk[y])
+            setup_field[p[0] - 2][p[1]] = Piece('R', [p[0] - 2, p[1]], general_maarschalk[y + 1])
+            possible_positions.remove([p[0], p[1]])
+            possible_positions.remove([p[0] - 2, p[1]])
+            general_placed = True
+        else: 
+            if [p[0], p[1]] in possible_positions:
+                setup_field[p[0]][p[1]] = Piece('R', [p[0], p[1]], spy_verkenner[g])
+                possible_positions.remove(p)
+                g += 1
+        if not possible_positions:
+            all_fields.append(setup_field)
+            print("")
+            Board(all_fields[0]).print_board()
+            z += 1
+            break
+    
+    if z == len(miner_positions): 
+        z = 0
+        j += 1
+    if j == 2:
+        break
 
-print(results)
+
+
+# sim = Simulator(10000)
+# #red_prob, blue_prob, draw_prob, average_moves, average_moves_red, average_moves_blue, spies_prob = sim.runSimulation([0, 1])
+# #print(red_prob, blue_prob, draw_prob, average_moves, average_moves_red, average_moves_blue, spies_prob)
+# results = {}
+#
+# for i in range(4):
+#     print(setup_field)
+#     results[(0 + i, 1)] = list(sim.runSimulation([0 + i, 1], setup_field))
+#
+# print(results)
 
 
 
